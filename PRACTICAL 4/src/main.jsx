4e3026007944655fd4551d5dabeebb77bef65f91
Dhruvil Patel <152694197+HetSoni916@users.noreply.github.com>
2025-09-29 16:38:23 +0530

    Remove node_modules from tracking and update repository

--- PRACTICAL 4/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Practical4 from '../Practical4.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Practical4 />
-  </React.StrictMode>,
-)
